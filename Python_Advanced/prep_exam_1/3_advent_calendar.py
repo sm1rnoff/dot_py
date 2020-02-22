12ee32d840d7365e19258da83681a52872ea6c74
vladimir <mail4vlado@gmail.com>
2020-02-22 17:25:06 +0200

    done with 3rd one from prep 1

---
+++ Python_Advanced/prep_exam_1/3_advent_calendar.py
@@ -0,0 +1,18 @@
+def fix_calendar(arr):
+    """ sort in ascending order using so called bubble sort algorithm """
+    def swap(i, j):
+        arr[i], arr[j] = arr[j], arr[i]
+
+    n = len(arr)
+    swapped = True
+
+    x = -1
+    while swapped:
+        swapped = False
+        x = x + 1
+        for i in range(1, n-x):
+            if arr[i - 1] > arr[i]:
+                swap(i - 1, i)
+                swapped = True
+
+    return arr

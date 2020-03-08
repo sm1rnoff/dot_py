c952d4d710c6aa030b9b671c702d911484d553e9
vladimir <mail4vlado@gmail.com>
2020-03-08 19:24:26 +0200

    done with classes-and-instances

--- Python OOP/2_classes_and_instances/exercises/5_time.py
+++ Python OOP/2_classes_and_instances/exercises/5_time.py
@@ -1,11 +1,12 @@
 class Time:
+    max_hours = 24
+    max_minutes = 60
+    max_seconds = 60
+
     def __init__(self, hours: int, minutes: int, seconds: int):
         self.hours = hours
         self.minutes = minutes
         self.seconds = seconds
-        self.max_hours = 24
-        self.max_minutes = 60
-        self.max_seconds = 60
 
     def set_time(self, hours, minutes, seconds):
         self.hours = hours
@@ -13,18 +14,29 @@ class Time:
         self.seconds = seconds
 
     def get_time(self):
-        print(f'{self.hours:02}:{self.minutes:02}:{self.seconds:02}')
+        return f'{self.hours:02}:{self.minutes:02}:{self.seconds:02}'
 
     def next_second(self):
-        if self.seconds == 60:
-            if self.minutes == 60:
-                if self.hours == 24:
-                    self.set_time(0, 0, 0)
-                self.set_time(self.hours+1, 0, 0)
-            self.set_time(self.hours, self.minutes+1, 0)
-        self.set_time(self.hours, self.minutes, self.seconds+1)
-        self.get_time()
+        if self.seconds + 1 >= self.max_seconds:
+            self.seconds = 0
+            if self.minutes + 1 >= self.max_minutes:
+                self.minutes = 0
+                if self.hours + 1 >= self.max_hours:
+                    self.hours = 0
+                else:
+                    self.hours += 1
+            else:
+                self.minutes += 1
+        else:
+            self.seconds += 1
+        return self.get_time()
 
 
 time = Time(9, 30, 60)
+print(time.next_second())
+
+time = Time(10, 59, 59)
+print(time.next_second())
+
+time = Time(24, 59, 59)
 print(time.next_second())
